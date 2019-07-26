0ba7722df30f44c3a8f839ec858d85931a494514
Matt Reetz <matthewcarlreetz@gmail.com>
2019-07-26 12:35:54 -0400

    Add filtering to events query

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -39,6 +39,16 @@ const schema = gql`
     email: String!
   }
 
+  input SpeakerInput {
+    firstname: String!
+    lastname: String!
+    title: String!
+    company: String!
+    avatar: String!
+    biography: String!
+    email: String!
+  }
+
   type Event {
     id: ID!
     title: String!
@@ -52,19 +62,65 @@ const schema = gql`
     aboutMessage: String
     speakers: [Speaker]
     speaker(id: ID!): Speaker
-    events: [Event]
+    events(filter: String): [Event]
     event(id: ID!): Event
   }
+
+  type Mutation {
+    createSpeaker(speaker: SpeakerInput!): Speaker
+  }
 `;
+
+const speakersForEvent = event => {
+  const allSpeakers = dbHelper.findAll("speakers");
+  return event.speakerIds.map(id =>
+    allSpeakers.find(speaker => speaker.id === id)
+  );
+};
 
 const resolvers = {
   Query: {
     aboutMessage: () => "THAT Conference was founded by this guy",
     speakers: () => dbHelper.findAll("speakers"),
     speaker: (_parent, { id }) => dbHelper.findOne("speakers", id),
-    events: () => dbHelper.findAll("events"),
-    event: (_parent, { id }) => dbHelper.findOne("events", id),
+    events: (_parent, { filter }) => {
+      const allEvents = dbHelper.findAll("events");
+      if (!filter || filter === "") return allEvents;
+
+      const eventsWithSpeakers = allEvents.map(event => ({
+        ...event,
+        speakers: speakersForEvent(event)
+      }));
+
+      const filtered = eventsWithSpeakers.filter(event => {
+        const speakers = event.speakers
+          .map(
+            speaker =>
+              `${speaker.firstname}${speaker.lastname}${speaker.biography}`
+          )
+          .join();
+
+        const searchString = [event.title, event.description, speakers]
+          .join()
+          .replace(/\s/g, "")
+          .toLowerCase();
+
+          const filterString = filter.replace(/\s/g, "").toLowerCase();
+
+        return searchString.includes(filterString);
+      });
+
+      return filtered;
+    },
+    event: (_parent, { id }) => dbHelper.findOne("events", id)
   },
+  Mutation: {
+    createSpeaker: (_parent, { speaker }) =>
+      dbHelper.create("speakers", speaker)
+  },
+  Event: {
+    speakers: event => speakersForEvent(event)
+  }
 };
 
 const apolloServer = new ApolloServer({ typeDefs: schema, resolvers });
