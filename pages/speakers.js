74dd95c4f710258d450b2580ac5e42b7de79af36
Tim Gremore <timgremore@gmail.com>
2019-08-02 15:30:04 -0500

    Remove placeholder data from Speakers page

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,35 +1,12 @@
 import Layout from "../components/Layout";
 import { ListGroup, ListGroupItem, Media } from "reactstrap";
 
-const avatarUrl = "https://api.adorable.io/avatars/64/";
-
-const speakers = [
-  {
-    id: 1,
-    firstname: "Matt",
-    lastname: "Reetz",
-    title: "Software Engineer",
-    company: "Headway",
-    avatar: `${avatarUrl}matt`,
-    biography: "Matt is a coding ninja."
-  },
-  {
-    id: 2,
-    firstname: "Tim",
-    lastname: "Gremore",
-    title: "Software Engineer",
-    company: "Headway",
-    avatar: `${avatarUrl}tim`,
-    biography: "Tim is a coding wizard."
-  }
-];
-
 const Speakers = () => {
   return (
     <Layout>
       <h1 className="title">Speakers</h1>
       <ListGroup>
-        {speakers.map(speaker => {
+        {[].map(speaker => {
           return (
             <ListGroupItem className="justify-content-between">
               <Media>
