7ea6c4b0a3b8ad8945dac293383cd24be8188d4c
Tim Gremore <timgremore@gmail.com>
2019-08-02 15:20:39 -0500

    Add a database to our application

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,6 +1,44 @@
 const { ApolloServer, gql } = require("apollo-server-micro");
+const low = require("lowdb");
+const uuidv4 = require("uuid");
+const os = require("os");
+const fs = require("fs");
+const FileSync = require("lowdb/adapters/FileSync");
+const database = require("../../db.js");
+
+const filePath = os.tmpdir() + "/database.json";
+fs.writeFileSync(filePath, JSON.stringify(database()));
+const adapter = new FileSync(filePath);
+const db = low(adapter);
+
+const dbHelper = {
+  findAll: type => db.get(type).value(),
+  findOne: (type, id) =>
+    db
+      .get(type)
+      .find({ id })
+      .value(),
+  create: (type, values) => {
+    const id = uuidv4();
+    db.get(type)
+      .push({ ...values, id })
+      .write();
+    return dbHelper.findOne(type, id);
+  }
+};
 
 const schema = gql`
+  type Speaker {
+    id: ID!
+    firstname: String!
+    lastname: String!
+    title: String!
+    company: String!
+    avatar: String!
+    biography: String!
+    email: String!
+  }
+
   type Query {
     aboutMessage: String
   }
@@ -8,10 +46,8 @@ const schema = gql`
 
 const resolvers = {
   Query: {
-    aboutMessage(parent, args, context) {
-      return 'THAT Conference was founded by this guy';
-    }
-  }
+    aboutMessage: () => "THAT Conference was founded by this guy",
+  },
 };
 
 const apolloServer = new ApolloServer({ typeDefs: schema, resolvers });
