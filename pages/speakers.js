200fabbc806cbf00f026f12b5e93067a3023a09b
Tim Gremore <timgremore@gmail.com>
2019-08-02 15:32:02 -0500

    Render API data on Speakers page

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,12 +1,33 @@
 import Layout from "../components/Layout";
 import { ListGroup, ListGroupItem, Media } from "reactstrap";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import withData from "../lib/apollo";
+
+const GET_SPEAKERS = gql`
+  query speakers {
+    speakers {
+      id
+      firstname
+      lastname
+      avatar
+      biography
+    }
+  }
+`;
 
 const Speakers = () => {
+  const { data: { speakers = [] } = {}, error } = useQuery(GET_SPEAKERS);
+
+  if (error) {
+    console.error(error);
+  }
+
   return (
     <Layout>
       <h1 className="title">Speakers</h1>
       <ListGroup>
-        {[].map(speaker => {
+        {speakers.map(speaker => {
           return (
             <ListGroupItem className="justify-content-between">
               <Media>
@@ -42,4 +63,4 @@ const Speakers = () => {
   );
 };
 
-export default Speakers;
+export default withData(Speakers);
