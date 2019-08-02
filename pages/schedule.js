7497b7e20408298962c584a261aee91aa2a62e2c
Tim Gremore <timgremore@gmail.com>
2019-08-02 16:17:53 -0500

    Remove placeholder data from schedule page

--- pages/schedule.js
+++ pages/schedule.js
@@ -2,22 +2,6 @@ import Layout from "../components/Layout";
 import { ListGroup, ListGroupItem, Media } from "reactstrap";
 
 const scheduleItems = [
-  {
-    id: "1",
-    title: "Conference Kickoff",
-    location: "Room 1",
-    description: "Conference Welcoming Party. Drinks and Food will be served.",
-    start: "2019-08-23T08:00:00.000Z",
-    end: "2019-08-23T18:00:00.000Z"
-  },
-  {
-    id: "2",
-    title: "Goodbye Conference",
-    location: "Room 2",
-    description: "Farewell. Hope you learned something.",
-    start: "2019-08-24T08:00:00.000Z",
-    end: "2019-08-24T18:00:00.000Z"
-  }
 ];
 
 const Schedule = () => {
